339754862616e535d84ecd48e8e6c15b60877500
raakaprx <raka101004@gmail.com>
2023-10-04 20:09:39 +0700

    initial project

---
+++ src/main/java/Buku2.java
@@ -0,0 +1,25 @@
+public class Buku2 {class Buku
+{
+    private String judul;
+    private String pengarang;
+    public Buku()
+    {
+        judul = "Tidak diketahui";
+        pengarang = "Tidak diketahui";
+    }
+    public Buku(String judul,String pengarang)
+    {
+        System.out.println
+                ("konstruktor buku sedang dijalankan...");
+        this.judul = judul;
+        this.pengarang = pengarang;
+    }
+    void cetakKeLayar()
+    {
+        if(judul==null && pengarang==null)
+            return;
+        System.out.println("Judul : " + judul );
+        System.out.println("Pengarang : " + pengarang );
+    }
+}
+}
